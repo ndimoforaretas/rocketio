dedb42f6a87d731dcb659a1fb0c31f4df310a2a0
Ndimofor <ndimaret@gmail.com>
2023-11-11 19:11:54 +0100

    edit script and web manifest file

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", initApp);
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
-      .register("/service_worker.js")
+      .register("/serviceWorker.js")
       .then((res) => console.log("service worker registered"))
       .catch((err) => console.log("service worker not registered", err));
   });
